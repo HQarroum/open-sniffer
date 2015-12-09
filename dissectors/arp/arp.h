c0ac31686a3748a83cd231d6cc73f0271facffbd
Halim Qarroum <hqm.post@gmail.com>
2015-12-09 20:09:47 +0100

    Documenting the ARP dissector interfaces

--- dissectors/arp/arp.h
+++ dissectors/arp/arp.h
@@ -1,5 +1,5 @@
-#ifndef _ARP_H_
-# define _ARP_H_
+#ifndef ARP_H
+#define ARP_H
 
 #include "../../osi.h"
 #include "../../termcaps.h"
@@ -19,9 +19,30 @@
   "-------------------------------------------------------\n"	\
   COLOR_END
 
-const osi_dissector_t*	arp_dissector_new();
-int			arp_dissector_handles(const packet_t* packet);
-void			arp_dissector_dump(const packet_t* packet);
-packet_t*		arp_dissector_decapsulate(const packet_t* packet);
+/**
+ * \brief Creates a new ARP dissector.
+ * \return an instance of a dissector
+ */
+const osi_dissector_t* arp_dissector_new();
+
+/**
+ * \return whether the dissector can handle the
+ * given packet.
+ * \see osi_dissector_t
+ */
+int  arp_dissector_handles(const packet_t* packet);
+
+/**
+ * \brief Dumps the given packet on the standard output.
+ * \see osi_dissector_t
+ */
+void arp_dissector_dump(const packet_t* packet);
+
+/**
+ * \brief Removes the bytes related to ARP in the given packet.
+ * \return a pointer to a packet with every bytes related to
+ * the ARP segment removed
+ */
+packet_t* arp_dissector_decapsulate(const packet_t* packet);
 
 #endif
