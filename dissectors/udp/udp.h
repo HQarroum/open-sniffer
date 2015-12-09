93a1e9eac03ca661786396f2e4f97286b48d9fdd
Halim Qarroum <hqm.post@gmail.com>
2015-12-09 20:34:58 +0100

    Documenting the UDP dissector interfaces

--- dissectors/udp/udp.h
+++ dissectors/udp/udp.h
@@ -1,5 +1,5 @@
-#ifndef _UDP_H_
-# define _UDP_H_
+#ifndef UDP_H
+#define UDP_H
 
 #include "../../osi.h"
 #include "../../termcaps.h"
@@ -18,9 +18,30 @@
   "-------------------------------------------------------\n"	\
   COLOR_END
 
+/**
+ * \brief Creates a new UDP dissector.
+ * \return an instance of a dissector
+ */
 const osi_dissector_t*	udp_dissector_new();
-int			udp_dissector_handles(const packet_t* packet);
-void			udp_dissector_dump(const packet_t* packet);
-packet_t*		udp_dissector_decapsulate(const packet_t* packet);
+
+/**
+ * \return whether the dissector can handle the
+ * given packet.
+ * \see osi_dissector_t
+ */
+int       udp_dissector_handles(const packet_t* packet);
+
+/**
+ * \brief Dumps the given packet on the standard output.
+ * \see osi_dissector_t
+ */
+void      udp_dissector_dump(const packet_t* packet);
+
+/**
+ * \brief Removes the bytes related to UDP in the given packet.
+ * \return a pointer to a packet with every bytes related to
+ * the UDP segment removed
+ */
+packet_t* udp_dissector_decapsulate(const packet_t* packet);
 
 #endif
