d9723b2e35a061fdcf62e54024868b1650e1c97c
Halim Qarroum <hqm.post@gmail.com>
2015-12-09 20:25:07 +0100

    Documenting the Ethernet dissector interfaces

--- dissectors/ethernet/ethernet.h
+++ dissectors/ethernet/ethernet.h
@@ -1,5 +1,5 @@
-#ifndef _ETHERNET_H_
-# define _ETHERNET_H_
+#ifndef ETHERNET_H
+#define ETHERNET_H
 
 #include "../../osi.h"
 #include "../../termcaps.h"
@@ -17,9 +17,30 @@
   "-------------------------------------------------------\n"	\
   COLOR_END
 
-const osi_dissector_t*	ethernet_dissector_new();
-int			ethernet_dissector_handles(const packet_t* packet);
+/**
+ * \brief Creates a new Ethernet dissector.
+ * \return an instance of a dissector
+ */
+const osi_dissector_t* ethernet_dissector_new();
+
+/**
+ * \return whether the dissector can handle the
+ * given packet.
+ * \see osi_dissector_t
+ */
+int			  ethernet_dissector_handles(const packet_t* packet);
+
+/**
+ * \brief Dumps the given packet on the standard output.
+ * \see osi_dissector_t
+ */
 void			ethernet_dissector_dump(const packet_t* packet);
-packet_t*		ethernet_dissector_decapsulate(const packet_t* packet);
+
+/**
+ * \brief Removes the bytes related to Ethernet in the given packet.
+ * \return a pointer to a packet with every bytes related to
+ * the Ethernet segment removed
+ */
+packet_t* ethernet_dissector_decapsulate(const packet_t* packet);
 
 #endif
