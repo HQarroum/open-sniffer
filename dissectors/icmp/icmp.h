c00e43d235d535c7378d16e417c77bf26bcde872
Halim Qarroum <hqm.post@gmail.com>
2015-12-09 20:29:48 +0100

    Documenting the ICMP dissector interfaces

--- dissectors/icmp/icmp.h
+++ dissectors/icmp/icmp.h
@@ -1,5 +1,5 @@
-#ifndef _ICMP_H_
-# define _ICMP_H_
+#ifndef ICMP_H
+#define ICMP_H
 
 #include "../../osi.h"
 #include "../../termcaps.h"
@@ -30,9 +30,30 @@ typedef struct icmp_dissector_packet_types_t
   const char* type_name;
 } icmp_dissector_packet_types_t;
 
-const osi_dissector_t*	icmp_dissector_new();
-int			icmp_dissector_handles(const packet_t* packet);
-void			icmp_dissector_dump(const packet_t* packet);
-packet_t*		icmp_dissector_decapsulate(const packet_t* packet);
+/**
+ * \brief Creates a new ICMP dissector.
+ * \return an instance of a dissector
+ */
+const osi_dissector_t* icmp_dissector_new();
+
+/**
+ * \return whether the dissector can handle the
+ * given packet.
+ * \see osi_dissector_t
+ */
+int       icmp_dissector_handles(const packet_t* packet);
+
+/**
+ * \brief Dumps the given packet on the standard output.
+ * \see osi_dissector_t
+ */
+void      icmp_dissector_dump(const packet_t* packet);
+
+/**
+ * \brief Removes the bytes related to ICMP in the given packet.
+ * \return a pointer to a packet with every bytes related to
+ * the ICMP segment removed
+ */
+packet_t* icmp_dissector_decapsulate(const packet_t* packet);
 
 #endif
