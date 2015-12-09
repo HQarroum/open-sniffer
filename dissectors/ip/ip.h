b5cce3807954cd1d1876a5159b7a4c8d4b0ea377
Halim Qarroum <hqm.post@gmail.com>
2015-12-09 20:31:38 +0100

    Documenting the IP dissector interfaces

--- dissectors/ip/ip.h
+++ dissectors/ip/ip.h
@@ -17,9 +17,30 @@
   "-------------------------------------------------------\n"	\
   COLOR_END
 
+/**
+ * \brief Creates a new IP dissector.
+ * \return an instance of a dissector
+ */
 const osi_dissector_t*	ip_dissector_new();
-int			ip_dissector_handles(const packet_t* packet);
-void			ip_dissector_dump(const packet_t* packet);
-packet_t*		ip_dissector_decapsulate(const packet_t* packet);
+
+/**
+ * \return whether the dissector can handle the
+ * given packet.
+ * \see osi_dissector_t
+ */
+int       ip_dissector_handles(const packet_t* packet);
+
+/**
+ * \brief Dumps the given packet on the standard output.
+ * \see osi_dissector_t
+ */
+void      ip_dissector_dump(const packet_t* packet);
+
+/**
+ * \brief Removes the bytes related to IP in the given packet.
+ * \return a pointer to a packet with every bytes related to
+ * the IP segment removed
+ */
+packet_t* ip_dissector_decapsulate(const packet_t* packet);
 
 #endif
